94259130a19867898a12225e6f66d17f542ffe4b
Fernando Luchiari <luchiari.fernando@gmail.com>
2021-10-31 19:09:02 -0300

    create log file

--- main.py
+++ main.py
@@ -4,38 +4,38 @@ import logging
 from binance.spot import Spot as Client
 from binance.lib.utils import config_logging
 
-def callapi(apikey,apisecret):
+logging.basicConfig(filename='error.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('This message will get logged on to a file')
 
-    spot_client = Client(apikey+'0', apisecret, show_header=True)
+def callapi(apikey, apisecret):
+    spot_client = Client(apikey, apisecret, show_header=True)
     try:
         response = spot_client.api_key_permissions()
-        keypermissresponse = json.loads(response.content)
+        keypermissresponse = response['data']
         return keypermissresponse
-    except:
-        print("Something went wrong")
+    except Exception as e:
+        print("Something went wrong:")
+        logging.error(e)
+        sys.exit()
 
 
-
-
-def validatePermissions(apikey,apisecret):
-    permissions = callapi(apikey,apisecret)
+def validatePermissions(apikey, apisecret):
+    permissions = callapi(apikey, apisecret)
     if (permissions['enableSpotAndMarginTrading']) and (not permissions['enableWithdrawals']):
         return True
     else:
         return False
 
 
-
 KeyArchive = 'keys.csv'
 with open(KeyArchive, 'rt') as archive:
-    print(archive)
-    reader = csv.reader(archive, delimiter=';')
-    for linha in reader:
-        print("user's apiKeys validation")
-        if(validatePermissions(linha[0], linha[1])
-
-
-
-
-
-
+    try:
+        reader = csv.reader(archive, delimiter=';')
+        for linha in reader:
+            print("user's apiKeys validation")
+            if validatePermissions(linha[0], linha[1]):
+                print("acesso ok")
+    except Exception as e:
+        print("Something went wrong with key's archive:")
+        logging.error(e)
+        sys.exit()
