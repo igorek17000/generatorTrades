e797af4615336a7266e9c68502405c2dc0195384
Fernando Luchiari <luchiari.fernando@gmail.com>
2021-11-14 13:08:51 -0300

    acessing sheets on google sheets

--- order.py
+++ order.py
@@ -1,41 +1,50 @@
 import csv
 import logging
 import sys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 from utils import configlog
 
-
-apikey= ''
+apikey = ''
 apisecret = ''
 usdtBalance = ''
 ordersprice = 0
 coinsLocked = []
+
 
 def organizedata(data):
     datas = data.split(';')
     apikey = datas[0]
     apisecret = datas[1]
     usdtBalance = datas[2]
-    ordersprice = float(usdtBalance)/4
-    for x in range(3, len(datas)-1):
+    ordersprice = float(usdtBalance) / 4
+    for x in range(3, len(datas) - 1):
         coinsLocked.append(datas[x])
+
+
 def retrievecoinsvalue():
-    keyarchive = 'coins.csv'
-    with open(keyarchive, 'rt') as archive:
-        try:
-            reader = csv.reader(archive, delimiter=';')
-            for linha in reader:
+    # use creds to create a client to interact with the Google Drive API
+    try:
+        scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
+
+        sheet = client.open("test_coins").sheet1
+
+        list_of_hashes = sheet.get_all_records()
 
 
-
-            return
-        except Exception as e:
-            print("Something went wrong with the list :" + e)
+    except Exception as e:
+            print("Something went wrong when retriving coins value" + e)
             logging.error(e)
             sys.exit()
 
+
+
+
+
 def createorder(data):
-    organizedata(data)
-    makeorder()
-
-
+    """organizedata(data)
+    makeorder()"""
