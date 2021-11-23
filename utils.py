c940529b2e60a238ca42a9dcda2745319efc5995
Fernando Luchiari <luchiari.fernando@gmail.com>
2021-11-22 23:02:02 -0300

    fix: creation log file

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import logging
 
 def configlog():
-    logging.basicConfig(filename='error.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='error.log', format='%(name)s - %(levelname)s - %(message)s')
     logging.warning('This message will get logged on to a file')
 
 
